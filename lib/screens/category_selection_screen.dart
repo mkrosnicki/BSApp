1c56615860f94508d241601ab2d23e0fc2a2896c
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-15 09:04:42 +0100

    categories selector - initial logic

--- lib/screens/category_selection_screen.dart
+++ lib/screens/category_selection_screen.dart
@@ -1,3 +1,4 @@
+import 'package:BSApp/models/category_model.dart';
 import 'package:BSApp/providers/categories.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -11,33 +12,62 @@ class CategorySelectionScreen extends StatefulWidget {
 }
 
 class _CategorySelectionScreenState extends State<CategorySelectionScreen> {
+  List<CategoryModel> _allCategories;
+  CategoryModel _selectedCategory;
+
+  Future<void> _initCategories() {
+    return Provider.of<Categories>(context, listen: false).fetchCategories().then((_) {
+      _allCategories = Provider.of<Categories>(context, listen: false).categories;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: Provider.of<Categories>(context, listen: false).fetchCategories(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            if (snapshot.error != null) {
-              return Center(
-                child: Text('An error occurred!'),
-              );
-            } else {
-              return Consumer<Categories>(
-                builder: (context, categoriesData, child) =>
-                    ListView.builder(
-                      itemBuilder: (context, index) =>
-                          Text(categoriesData.categories[index].name),
-                      itemCount: categoriesData.categories.length,
-                    ),
-              );
-            }
-          }
-        },
-      )
-      ,
+      body: _selectedCategory == null
+          ? FutureBuilder(
+              future: _initCategories(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  if (snapshot.error != null) {
+                    return Center(
+                      child: Text('An error occurred!'),
+                    );
+                  } else {
+                    return _buildCategoriesList(_allCategories);
+                  }
+                }
+              },
+            )
+          : _buildCategoriesList(_selectedCategory.subCategories),
     );
+  }
+
+  _buildCategoriesList(List<CategoryModel> categories) {
+    return ListView.builder(
+      itemBuilder: (context, index) => FlatButton(
+        child: ListTile(
+          title: Text(categories[index].name),
+          subtitle: Text('${categories[index].subCategories.length}'),
+          trailing: Icon(Icons.chevron_right),
+          focusColor: Colors.grey,
+        ),
+        onPressed: () =>
+            _selectCategory(categories[index]),
+      ),
+      itemCount: categories.length,
+    );
+  }
+
+  _selectCategory(CategoryModel category) {
+    if (category.subCategories.isEmpty) {
+      Navigator.of(context).pop(category);
+    } else {
+      setState(() {
+        _selectedCategory = category;
+      });
+    }
   }
 }
