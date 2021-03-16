b22e4983e984485c50524079cff1dd3e170afdb3
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-16 20:22:40 +0100

    filter selection screen initial layout

--- lib/screens/deals/filter_selection_screen.dart
+++ lib/screens/deals/filter_selection_screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
 class FilterSelectionScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(elevation: 10,),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: RaisedButton(
-            child: Text('Akceptuj'),
-            onPressed: () => _acceptFilters(context),
+      appBar: AppBar(
+        title: Text('Filtry i sortowanie'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: RaisedButton(
+                child: Text('Akceptuj'),
+                onPressed: () => _acceptFilters(context),
+              ),
+            ),
           ),
-        ),
+          Card(
+            elevation: 10,
+            margin: EdgeInsets.zero,
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+              child: RaisedButton(
+                child: Text('Filtruj'),
+                onPressed: () => _acceptFilters(context),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
