90ab4ef2b29cf1d8cbaa9b1a1b14e261708d2c11
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-16 13:13:10 +0100

    adding comments part 7

--- lib/widgets/comment_item.dart
+++ lib/widgets/comment_item.dart
@@ -10,47 +10,67 @@ class CommentItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 40,
-                height: 40,
-                child: Image.network(
-                    'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg'),
-              ),
-              Text(comment.username),
-            ],
+    return Column(
+      children: [
+        _buildComment(comment),
+        Padding(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Column(
+            children: comment.subComments.map((reply) => _buildComment(reply)).toList(),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 8.0),
-            child: Text(comment.content),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              GestureDetector(
-                onTap: () => setCommentModeFunction(CommentMode.REPLY_COMMENT, commentToReplyId: comment.id),
-                child: Text(
-                  'Odpowiedz',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  void _replyToComment() {}
+  Widget _buildComment(CommentModel comment) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 40,
+                    height: 40,
+                    child: Image.network(
+                        'https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg'),
+                  ),
+                  Text(comment.username),
+                ],
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 22.0, vertical: 8.0),
+                child: Text(comment.content),
+              ),
+              if (comment.parentId == null)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () => setCommentModeFunction(CommentMode.REPLY_COMMENT,
+                          commentToReplyId: comment.id),
+                      child: Text(
+                        'Odpowiedz',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ],
+          ),
+        ),
+        Divider(),
+      ],
+    );
+  }
 }
