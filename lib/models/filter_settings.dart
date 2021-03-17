26d64f0ba4f6f4fd02453a4837062b396b8156b1
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-17 14:15:34 +0100

    location selection - logic added

--- lib/models/filter_settings.dart
+++ lib/models/filter_settings.dart
@@ -1,13 +1,21 @@
 import 'package:BSApp/models/category_model.dart';
+import 'package:BSApp/models/city_model.dart';
+import 'package:BSApp/models/voivodeship_model.dart';
 
 class FilterSettings {
 
   String phrase;
   List<CategoryModel> categories;
   bool showActiveOnly = false;
+  Voivodeship voivodeship;
+  City city;
 
   String get categoriesString {
     return categories.map((e) => e.name).join(" / ");
+  }
+
+  String get locationString {
+    return voivodeship != null ? '${voivodeship.name} / ${city != null ? city.name : 'Wszystkie miasta'}' : null;
   }
 
 }
