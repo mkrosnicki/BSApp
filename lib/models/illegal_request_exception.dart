b7072215d1f611d4f7f8bbd731d6821b8a0cd018
Marek Marciński <marcinski.mar@gmail.com>
2021-03-18 08:01:47 +0100

    reformatted ApiProvider

---
+++ lib/models/illegal_request_exception.dart
@@ -0,0 +1,11 @@
+
+class IllegalRequestException implements Exception {
+  final String message;
+
+  IllegalRequestException(this.message);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
--- lib/services/api_provider.dart
+++ lib/services/api_provider.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:BSApp/models/custom_exception.dart';
+import 'package:BSApp/models/illegal_request_exception.dart';
 import 'package:http/http.dart' as http;
 
 class ApiProvider {
-
   String token;
 
   ApiProvider({this.token});
@@ -13,47 +13,48 @@ class ApiProvider {
 
 
   Future<dynamic> get(String url, {String token}) async {
-    var responseJson;
-    try {
-      Map<String, String> headers = {};
-      if (token != null) {
-        headers.putIfAbsent("Authorization", () => 'Bearer $token');
-      }
-      final response = await http.get(_baseUrl + url, headers: headers);
-      responseJson = _response(response);
-    } on SocketException {
-      throw FetchDataException('No Internet connection');
-    }
-    return responseJson;
+    return _sendRequest(_RequestType.GET, url, token: token);
   }
 
-  Future<dynamic> post(String url, Map<String, dynamic> body, {String token}) async {
-    var responseJson;
-    try {
-      Map<String, String> headers = {"Content-Type": "application/json"};
-      if (token != null) {
-        headers.putIfAbsent("Authorization", () => 'Bearer $token');
-      }
-      final response = await http.post(
-        _baseUrl + url,
-        body: json.encode(body),
-        headers: headers,
-      );
-      responseJson = _response(response);
-    } on SocketException {
-      throw FetchDataException('No Internet connection');
-    }
-    return responseJson;
+  Future<dynamic> post(String url, Map<String, dynamic> body,
+      {String token}) async {
+    return _sendRequest(_RequestType.POST, url, body: body, token: token);
   }
 
   Future<dynamic> delete(String url, {String token}) async {
+    return _sendRequest(_RequestType.DELETE, url, token: token);
+  }
+
+  Future<dynamic> _sendRequest(_RequestType requestType, String url,
+      {Map<String, dynamic> body, String token}) async {
     var responseJson;
     try {
       Map<String, String> headers = {};
       if (token != null) {
         headers.putIfAbsent("Authorization", () => 'Bearer $token');
       }
-      final response = await http.delete(_baseUrl + url, headers: headers);
+      if (requestType == _RequestType.POST) {
+        headers.putIfAbsent('Content-Type', () => 'application/json');
+      }
+      var response;
+      switch (requestType) {
+        case _RequestType.DELETE:
+          response = await http.delete(_baseUrl + url, headers: headers);
+          break;
+        case _RequestType.POST:
+          response = await http.post(
+            _baseUrl + url,
+            body: json.encode(body),
+            headers: headers,
+          );
+          break;
+        case _RequestType.GET:
+          response = await http.get(_baseUrl + url, headers: headers);
+          break;
+        default:
+          throw IllegalRequestException(
+              'Request $requestType is not supported!');
+      }
       responseJson = _response(response);
     } on SocketException {
       throw FetchDataException('No Internet connection');
@@ -81,3 +82,5 @@ class ApiProvider {
     }
   }
 }
+
+enum _RequestType { GET, POST, DELETE }
