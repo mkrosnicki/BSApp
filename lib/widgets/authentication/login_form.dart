5f560e7f54f7aac902ce7da4410a9a0453cd7e47
Marek Marciński <marcinski.mar@gmail.com>
2021-03-18 10:34:41 +0100

    improved login form

--- lib/widgets/authentication/login_form.dart
+++ lib/widgets/authentication/login_form.dart
@@ -1,20 +1,21 @@
-import 'package:BSApp/models/http_exception.dart';
+import 'package:BSApp/models/custom_exception.dart';
 import 'package:BSApp/providers/auth.dart';
 import 'package:BSApp/util/util_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class LoginForm extends StatefulWidget {
-
   @override
   _LoginFormState createState() => _LoginFormState();
 }
 
 class _LoginFormState extends State<LoginForm> {
-
   final GlobalKey<FormState> _formKey = GlobalKey();
   Map<String, String> _authData = {'email': '', 'password': ''};
   bool _isLoading = false;
+
+  var _emailController = TextEditingController();
+  var _passwordController = TextEditingController();
 
   var formFieldDecoration = InputDecoration(
     enabledBorder: InputBorder.none,
@@ -39,55 +40,41 @@ class _LoginFormState extends State<LoginForm> {
       _isLoading = true;
     });
     try {
-      // TODO login vs signup
-      // if (_authMode == AuthMode.Login) {
-      if (true) {
-        // Log user in
-        await Provider.of<Auth>(context, listen: false).login(
-          _authData['email'],
-          _authData['password'],
-        );
-      } else {
-        // Sign user up
-        await Provider.of<Auth>(context, listen: false).signup(
-          _authData['email'],
-          _authData['password'],
-        );
-      }
-    } on HttpException catch (error) {
-      var errorMessage = 'Authentication failed';
-      if (error.toString().contains('EMAIL_EXISTS')) {
-        errorMessage = 'This email address is already in use.';
-      } else if (error.toString().contains('INVALID_EMAIL')) {
-        errorMessage = 'This is not a valid email address';
-      } else if (error.toString().contains('WEAK_PASSWORD')) {
-        errorMessage = 'This password is too weak.';
-      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
-        errorMessage = 'Could not find a user with that email.';
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Auth>(context, listen: false).login(
+        _authData['email'],
+        _authData['password'],
+      );
+    } on CustomException catch (error) {
+      var errorMessage =
+          'Logowanie zakończyło się niepowodzeniem. Spróbuj później.';
+      if (error.toString().contains('must be a well-formed email address')) {
+        errorMessage = 'Email w złym w formacie.';
       } else if (error.toString().contains('Unauthorized')) {
-        errorMessage = 'Nieprawidłowe hasło lub login.';
+        errorMessage = 'Nieprawidłowe hasło i / lub login.';
       }
-      _showErrorDialog(errorMessage);
+      await _showErrorDialog(errorMessage);
     } catch (error) {
       const errorMessage =
-          'Could not authenticate you. Please try again later.';
-      _showErrorDialog(errorMessage);
+          'Logowanie zakończyło się niepowodzeniem. Spróbuj później.';
+      await _showErrorDialog(errorMessage);
     }
 
     setState(() {
       _isLoading = false;
     });
-    Navigator.of(context).pop();
   }
 
-  void _showErrorDialog(String message) {
-    showDialog(
+  Future<void> _showErrorDialog(String message) async {
+    await showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
         title: Text('Błąd logowania'),
         content: Text(message),
         actions: <Widget>[
-          FlatButton(
+          TextButton(
             child: Text('Ok'),
             onPressed: () {
               Navigator.of(ctx).pop();
@@ -100,69 +87,77 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                alignment: Alignment.centerLeft,
-                child: const Text('E-mail'),
-              ),
-              TextFormField(
-                cursorColor: Colors.black,
-                keyboardType: TextInputType.emailAddress,
-                decoration: formFieldDecoration,
-                validator: (value) {
-                  if (value.isEmpty || !value.contains('@')) {
-                    return 'Nieprawidłowy e-mail!';
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _authData['email'] = value;
-                },
-              ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                alignment: Alignment.centerLeft,
-                child: const Text('Hasło'),
-              ),
-              TextFormField(
-                cursorColor: Colors.black,
-                obscureText: true,
-                decoration: formFieldDecoration,
-                validator: (value) {
-                  if (value.length < 3) {
-                    return 'Za krótkie hasło!';
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  _authData['password'] = value;
-                },
-              ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                alignment: Alignment.centerLeft,
-                child: const Text('Nie pamiętasz hasła?'),
-              ),
-              Container(
-                width: double.infinity,
-                child: RaisedButton(
-                  child: Text('Zaloguj się'),
-                  onPressed: _submit,
+    return _isLoading
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : SingleChildScrollView(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      alignment: Alignment.centerLeft,
+                      child: const Text('E-mail'),
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      cursorColor: Colors.black,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: formFieldDecoration,
+                      validator: (value) {
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Nieprawidłowy e-mail!';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {
+                        _authData['email'] = value;
+                      },
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      alignment: Alignment.centerLeft,
+                      child: const Text('Hasło'),
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      cursorColor: Colors.black,
+                      obscureText: true,
+                      decoration: formFieldDecoration,
+                      validator: (value) {
+                        if (value.length < 3) {
+                          return 'Za krótkie hasło!';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (value) {
+                        _authData['password'] = value;
+                      },
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                          'Nie pamiętasz hasła? - Trudno, masz problem :D'), // TODO
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        child: Text('Zaloguj się'),
+                        onPressed: _submit,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
