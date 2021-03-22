545bc5001f376258590edfe4a98aa1d4b15a54ce
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-22 07:37:20 +0100

    equals method for category

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -13,4 +13,14 @@ class CategoryModel {
       subCategories: (categorySnapshot['subCategories'] as List).map((e) => CategoryModel.of(e)).toList()
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryModel &&
+          runtimeType == other.runtimeType &&
+          id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
