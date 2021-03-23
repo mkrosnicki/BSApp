a58844bd4296739e063be5ff4c86442fb02dfbbe
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-23 14:39:03 +0100

    small gui improvement

--- lib/widgets/deals/deal_item.dart
+++ lib/widgets/deals/deal_item.dart
@@ -11,7 +11,7 @@ class DealItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 0.0),
+      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
       color: Colors.white,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -22,7 +22,7 @@ class DealItem extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Flexible(
-                flex: 40,
+                flex: 35,
                 child: Align(
                   alignment: Alignment.topLeft,
                   child: GestureDetector(
@@ -31,16 +31,48 @@ class DealItem extends StatelessWidget {
                           DealDetailsScreen.routeName,
                           arguments: deal.id);
                     },
-                    child: Image.network(
-                        'https://dadi-shop.pl/img/sklep-z-w%C3%B3zkami-dla-dzieci-g%C5%82%C4%99bokie-spacerowe-dadi-shop-logo-1526467719.jpg'),
+                    child: Stack(
+                      fit: StackFit.loose,
+                      children: [
+                        Container(
+                          height: 100,
+                          child: Image.network(
+                            'https://dadi-shop.pl/img/sklep-z-w%C3%B3zkami-dla-dzieci-g%C5%82%C4%99bokie-spacerowe-dadi-shop-logo-1526467719.jpg',
+                          ),
+                        ),
+                        Positioned(
+                          right: 8,
+                          bottom: 0,
+                          child: Container(
+                            padding: EdgeInsets.all(2.0),
+                            // color: Theme.of(context).accentColor,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              color: Theme.of(context).accentColor,
+                            ),
+                            constraints: BoxConstraints(
+                              minWidth: 16,
+                              minHeight: 16,
+                            ),
+                            child: Text(
+                              'value',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
               Flexible(
-                flex: 60,
+                flex: 65,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
-                      vertical: 0.0, horizontal: 10.0),
+                      vertical: 10.0, horizontal: 10.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -51,12 +83,27 @@ class DealItem extends StatelessWidget {
                         child: Padding(
                           padding: const EdgeInsets.symmetric(
                               vertical: 8.0, horizontal: 0.0),
-                          child: Text(
-                            deal.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          child: Flex(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            direction: Axis.horizontal,
+                            children: [
+                              Flexible(
+                                flex: 8,
+                                child: Text(
+                                  deal.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  // overflow: TextOverflow.clip,
+                                ),
+                              ),
+                              Flexible(
+                                flex: 2,
+                                child: Icon(Icons.favorite_border),
+                              ),
+                            ],
                           ),
                         ),
                       ),
@@ -67,10 +114,11 @@ class DealItem extends StatelessWidget {
                           style: TextStyle(color: Colors.blue),
                         ),
                       ),
-                      Text(deal.regularPrice.toString()),
-                      Text(deal.regularPrice.toString()),
                       Row(
                         children: [
+                          Text('${deal.regularPrice.toString()} zł'),
+                          Padding(
+                              padding: EdgeInsets.all(4.0), child: Text('•')),
                           Text('${deal.currentPrice.toString()} zł'),
                           Padding(
                               padding: EdgeInsets.all(4.0), child: Text('•')),
@@ -84,44 +132,13 @@ class DealItem extends StatelessWidget {
               ),
             ],
           ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 8.0),
-            child: Container(
-              color: Color.fromRGBO(249, 250, 251, 1),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 0.0, horizontal: 8.0),
-                      child: Icon(
-                        Icons.favorite_border,
-                        size: 16,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 0.0, horizontal: 8.0),
-                      child: Icon(
-                        Icons.mark_chat_unread_outlined,
-                        size: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
         ],
       ),
     );
   }
 
   _navigateToUserProfileScreen(BuildContext context) {
-    Navigator.of(context).pushNamed(UserProfileScreen.routeName, arguments: deal.addedById);
+    Navigator.of(context)
+        .pushNamed(UserProfileScreen.routeName, arguments: deal.addedById);
   }
 }
