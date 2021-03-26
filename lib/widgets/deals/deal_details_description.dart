369c74f61d2aa9027d17725369e08e6a5a41c968
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-26 13:28:45 +0100

    deal details screen styling

--- lib/widgets/deals/deal_details_description.dart
+++ lib/widgets/deals/deal_details_description.dart
@@ -11,38 +11,55 @@ class DealDetailsDescription extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 0.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(bottom: 4.0),
             child: Text(
               deal.title,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            textBaseline: TextBaseline.alphabetic,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            children: [
+              Text(
+                '${deal.currentPrice.toString()} zł ',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '${deal.regularPrice} zł',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black54,
+                    decoration: TextDecoration.lineThrough),
+              ),
+            ],
+          ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 10.0),
+            padding: const EdgeInsets.only(top: 10.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(deal.dealType.toString()),
-                Text(deal.categories.last),
-                Text(deal.regularPrice.toString()),
-                Row(
-                  children: [
-                    Text('${deal.currentPrice.toString()} zł'),
-                    Padding(padding: EdgeInsets.all(4.0), child: Text('•')),
-                    Text(deal.discountString),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 2.0),
+                  child: Text('Opis', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
                 ),
                 Text(deal.description),
               ],
             ),
-          )
-        ],
+          )        ],
       ),
     );
   }
