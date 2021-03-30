3f0e11de98c5503df8c0bf774a914578435a37e5
mkrosnicki <maciejkrosnicki@gmail.com>
2021-03-30 15:14:55 +0200

    forum screen initial layout

--- lib/screens/forum/topic_screen.dart
+++ lib/screens/forum/topic_screen.dart
@@ -2,12 +2,25 @@ import 'package:BSApp/util/my_styling_provider.dart';
 import 'package:BSApp/widgets/bars/app_bar_back_button.dart';
 import 'package:BSApp/widgets/bars/app_bar_bottom_border.dart';
 import 'package:BSApp/widgets/bars/app_bar_close_button.dart';
-import 'package:BSApp/widgets/bars/my_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TopicScreen extends StatelessWidget {
+class TopicScreen extends StatefulWidget {
   static const routeName = '/topic';
+
+  @override
+  _TopicScreenState createState() => _TopicScreenState();
+}
+
+class _TopicScreenState extends State<TopicScreen> {
+
+  bool isInReplyState;
+
+
+  @override
+  void initState() {
+    isInReplyState = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,24 +40,78 @@ class TopicScreen extends StatelessWidget {
           const AppBarCloseButton(Colors.black),
         ],
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Text(
-              'Temat',
-              style: TextStyle(fontSize: 22),
-            ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => {
+          setState(() {
+            FocusScope.of(context).unfocus();
+            isInReplyState = false;
+          })
+        },
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+          padding: const EdgeInsets.all(0),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Text(
+                            'Temat',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                          Text(
+                            'Temat',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                          Text(
+                            'Temat',
+                            style: TextStyle(fontSize: 22),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  if (isInReplyState) Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.1,
+                    color: Colors.white,
+                    child: Center(
+                      child: TextField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          labelText: 'Napisz komentarz',
+                          fillColor: Colors.white,
+                          filled: true,
+                        ),
+                        onChanged: (value) {
+                          // setState(() {
+                          //   _commentText = value;
+                          // });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: isInReplyState ? null : FloatingActionButton(
         onPressed: () {
-          // Add your onPressed code here!
+          setState(() {
+            isInReplyState = true;
+          });
         },
         child: const Icon(CupertinoIcons.reply),
         backgroundColor: Colors.deepOrange,
       ),
-      bottomNavigationBar: MyNavigationBar(1),
+      // bottomNavigationBar: MyNavigationBar(1),
     );
   }
 }
