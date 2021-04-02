b8fd145100f153407e6e09e7cfc0b5d231623d05
Marek Marciński <marcinski.mar@gmail.com>
2021-04-02 15:24:16 +0200

    added tabs for occasion and coupon in add deal screen

--- lib/widgets/bars/app_bar_add_deal.dart
+++ lib/widgets/bars/app_bar_add_deal.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'app_bar_bottom_border.dart';
 
 class AppBarAddDeal extends StatelessWidget implements PreferredSizeWidget {
-
-  static const Size _aPreferredSize = Size.fromHeight(50.0);
+  static const Size _aPreferredSize = Size.fromHeight(100.0);
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +17,45 @@ class AppBarAddDeal extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       elevation: 0,
       backgroundColor: Colors.white,
-      bottom: const AppBarBottomBorder(),
+      bottom: TabBar(
+          indicatorWeight: 3.0,
+          unselectedLabelStyle:
+              TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+          indicatorSize: TabBarIndicatorSize.tab,
+          tabs: [
+            _buildTabBar('Okazja'),
+            _buildTabBar('Kupon'),
+          ]),
     );
   }
 
   @override
   // TODO: implement preferredSize
   Size get preferredSize => _aPreferredSize;
+
+  _buildTabBar(String label) {
+    return Tab(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              // decoration: BoxDecoration(
+              //     border: Border(
+              //         bottom: BorderSide(color: MyColorsProvider.GREY_BORDER_COLOR, width: 0.5))),
+              // padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 0.0),
+              child: Center(
+                  child: Text(
+                label,
+                style: TextStyle(
+                  color: Colors.black87,
+                ),
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
