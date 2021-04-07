d84877288a104e82ea880b90bb7e063a9d1232b3
mkrosnicki <maciejkrosnicki@gmail.com>
2021-04-07 12:56:34 +0200

    small css alignements

--- lib/widgets/common/secondary-button.dart
+++ lib/widgets/common/secondary-button.dart
@@ -1,22 +1,23 @@
 import 'package:BSApp/util/my_colors_provider.dart';
 import 'package:flutter/material.dart';
 
-class PrimaryButton extends StatelessWidget {
+class SecondaryButton extends StatelessWidget {
   final String label;
   final Function function;
   final double fontSize;
 
-  const PrimaryButton(this.label, this.function, {this.fontSize});
+  const SecondaryButton(this.label, this.function, {this.fontSize});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(MyColorsProvider.BLUE),
+        elevation: MaterialStateProperty.all(0.0),
+        backgroundColor: MaterialStateProperty.all(Colors.white),
       ),
       child: Text(
         label,
-        style: TextStyle(fontSize: fontSize),
+        style: TextStyle(color: MyColorsProvider.BLUE, fontSize: fontSize),
       ),
       onPressed: function,
     );
--- lib/widgets/searches/observed_search_item.dart
+++ lib/widgets/searches/observed_search_item.dart
@@ -1,6 +1,8 @@
 import 'package:BSApp/models/search_model.dart';
 import 'package:BSApp/providers/searches.dart';
 import 'package:BSApp/screens/deals/deal_search_result_screen.dart';
+import 'package:BSApp/widgets/common/primary-button.dart';
+import 'package:BSApp/widgets/common/secondary-button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -20,19 +22,7 @@ class ObservedSearchItem extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _buildItem('Szukana fraza', searchModel.phrase),
-              GestureDetector(
-                onTap: () {
-                  Provider.of<Searches>(context, listen: false)
-                      .deleteSearch(searchModel.id);
-                },
-                child: Icon(CupertinoIcons.clear),
-              ),
-            ],
-          ),
+          _buildItem('Szukana fraza', searchModel.phrase),
           _buildItem('Kategoria', filterSettings.categoriesString),
           _buildItem('Lokalizacja', filterSettings.locationString),
           _buildItem('Wiek dziecka', filterSettings.ageTypesString),
@@ -43,16 +33,22 @@ class ObservedSearchItem extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushNamed(
-                      DealSearchResultScreen.routeName,
-                      arguments: searchModel.toFilterSettings());
+              SecondaryButton(
+                'Przestań obserwować',
+                () {
+                  Provider.of<Searches>(context, listen: false)
+                      .deleteSearch(searchModel.id);
                 },
-                child: ElevatedButton(
-                  child: Text('Zobacz wyniki'),
-                ),
+                fontSize: 12,
               ),
+              PrimaryButton(
+                'Zobacz wyniki',
+                () => Navigator.of(context).pushNamed(
+                  DealSearchResultScreen.routeName,
+                  arguments: searchModel.toFilterSettings(),
+                ),
+                fontSize: 12,
+              )
             ],
           ),
         ],
