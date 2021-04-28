4d701ce9470c3673586cc69c7c96d571358ba024
mkrosnicki <maciejkrosnicki@gmail.com>
2021-04-28 08:22:36 +0200

    deal item extracted widget

---
+++ lib/widgets/deals/deal_item_heart_button.dart
@@ -0,0 +1,46 @@
+import 'package:BSApp/models/deal_model.dart';
+import 'package:BSApp/providers/auth.dart';
+import 'package:BSApp/providers/deals.dart';
+import 'package:BSApp/screens/authentication/auth_screen_provider.dart';
+import 'package:BSApp/util/my_colors_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class DealItemHeartButton extends StatelessWidget {
+
+  final DealModel deal;
+
+  DealItemHeartButton(this.deal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Auth>(
+        builder: (context, authData, child) => Consumer<Deals>(
+      builder: (context, dealsData, child) {
+        return GestureDetector(
+          onTap: () => _toggleFavourites(
+              context,
+              deal,
+              dealsData.isObservedDeal(deal),
+              authData.isAuthenticated),
+          child: dealsData.isObservedDeal(deal)
+              ? Icon(CupertinoIcons.heart_fill, size: 20, color: MyColorsProvider.LIGHT_RED_SHADY)
+              : Icon(CupertinoIcons.heart_fill, size: 20, color: MyColorsProvider.LIGHT_GRAY),
+        );
+      },
+    ),);
+  }
+
+  _toggleFavourites(BuildContext context, DealModel deal, bool isFavourite,
+      bool isUserLoggedIn) {
+    if (!isUserLoggedIn) {
+      AuthScreenProvider.showLoginScreen(context);
+    } else if (isFavourite) {
+      Provider.of<Deals>(context, listen: false)
+          .deleteFromObservedDeals(deal.id);
+    } else {
+      Provider.of<Deals>(context, listen: false).addToObservedDeals(deal.id);
+    }
+  }
+}
--- lib/widgets/deals/deal_item_top_section.dart
+++ lib/widgets/deals/deal_item_top_section.dart
@@ -1,11 +1,8 @@
 import 'package:BSApp/models/deal_model.dart';
-import 'package:BSApp/providers/auth.dart';
-import 'package:BSApp/providers/deals.dart';
-import 'package:BSApp/screens/authentication/auth_screen_provider.dart';
 import 'package:BSApp/screens/deals/deal_details_screen.dart';
+import 'package:BSApp/widgets/deals/deal_item_heart_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class DealItemTopSection extends StatelessWidget {
   final DealModel deal;
@@ -14,85 +11,58 @@ class DealItemTopSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Auth>(
-      builder: (context, authData, child) => Flex(
-        direction: Axis.vertical,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () => Navigator.of(context)
-                    .pushNamed(DealDetailsScreen.routeName, arguments: deal),
-                child: Flex(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  direction: Axis.horizontal,
-                  children: [
-                    Flexible(
-                      flex: 8,
-                      child: Text(
-                        deal.title,
-                        style: const TextStyle(fontSize: 13),
-                        overflow: TextOverflow.clip,
-                      ),
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () => Navigator.of(context)
+                  .pushNamed(DealDetailsScreen.routeName, arguments: deal),
+              child: Flex(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(
+                    flex: 8,
+                    child: Text(
+                      deal.title,
+                      style: const TextStyle(fontSize: 13),
+                      overflow: TextOverflow.clip,
                     ),
-                    Flexible(
-                      flex: 2,
-                      child: Consumer<Deals>(
-                        builder: (context, dealsData, child) {
-                          return GestureDetector(
-                            onTap: () => _toggleFavourites(
-                                context,
-                                deal,
-                                dealsData.isObservedDeal(deal),
-                                authData.isAuthenticated),
-                            child: dealsData.isObservedDeal(deal)
-                                ? Icon(CupertinoIcons.heart_fill, size: 20)
-                                : Icon(CupertinoIcons.heart, size: 20),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                  Flexible(
+                    flex: 2,
+                    child: DealItemHeartButton(deal),
+                  ),
+                ],
               ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            textBaseline: TextBaseline.alphabetic,
-            crossAxisAlignment: CrossAxisAlignment.baseline,
-            children: [
-              Text(
-                '${deal.currentPrice.toString()} zł ',
-                style: TextStyle(
-                    color: Colors.blue.shade500,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600),
-              ),
-              Text(
-                '${deal.regularPrice} zł',
-                style: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                    decoration: TextDecoration.lineThrough),
-              ),
-            ],
-          )
-        ],
-      ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          textBaseline: TextBaseline.alphabetic,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          children: [
+            Text(
+              '${deal.currentPrice.toString()} zł ',
+              style: TextStyle(
+                  color: Colors.blue.shade500,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              '${deal.regularPrice} zł',
+              style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                  decoration: TextDecoration.lineThrough),
+            ),
+          ],
+        )
+      ],
     );
-  }
-
-  _toggleFavourites(BuildContext context, DealModel deal, bool isFavourite,
-      bool isUserLoggedIn) {
-    if (!isUserLoggedIn) {
-      AuthScreenProvider.showLoginScreen(context);
-    } else if (isFavourite) {
-      Provider.of<Deals>(context, listen: false)
-          .deleteFromObservedDeals(deal.id);
-    } else {
-      Provider.of<Deals>(context, listen: false).addToObservedDeals(deal.id);
-    }
   }
 }
