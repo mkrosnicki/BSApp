0d0245ee1ced41d4dcbe53c44ff3659640ae6657
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-04 10:23:13 +0200

    server communication error splash

--- lib/screens/notifications/notifications_screen.dart
+++ lib/screens/notifications/notifications_screen.dart
@@ -1,5 +1,6 @@
 import 'package:BSApp/providers/notifications.dart';
 import 'package:BSApp/widgets/bars/base_app_bar.dart';
+import 'package:BSApp/widgets/common/server_error_splash.dart';
 import 'package:BSApp/widgets/notifications/notifications_item.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -26,7 +27,7 @@ class NotificationsScreen extends StatelessWidget {
             } else {
               if (snapshot.error != null) {
                 return Center(
-                  child: Text('An error occurred!'),
+                  child: const ServerErrorSplash(),
                 );
               } else {
                 return RefreshIndicator(
---
+++ lib/widgets/common/server_error_splash.dart
@@ -0,0 +1,33 @@
+import 'package:BSApp/util/my_colors_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ServerErrorSplash extends StatelessWidget {
+
+  const ServerErrorSplash();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Wrap(
+          direction: Axis.vertical,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            const Icon(CupertinoIcons.wifi_slash, color: MyColorsProvider.LIGHT_GRAY, size: 60),
+            const Text(
+              'Błąd komunikacji z serwerem',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 18,
+                  height: 2,
+                  fontWeight: FontWeight.w600,
+                  color: MyColorsProvider.LIGHT_GRAY),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
