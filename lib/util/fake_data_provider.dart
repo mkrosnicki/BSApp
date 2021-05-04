aac8f12f22e6ac9f25160b00bceb3aa3463802ae
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-04 21:58:06 +0200

    corrected user image

--- lib/util/fake_data_provider.dart
+++ lib/util/fake_data_provider.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
 
-import 'my_colors_provider.dart';
 
 class FakeDataProvider {
-  static const String USER_AVATAR_PATH = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTghSGsSSG2lZU9fuWAm4vrrmPAV15hrLius3_0Z7C45QrYf17F2x7VzrdT1XVXMV-z4EM&usqp=CAU';
+  static const String USER_AVATAR_PATH =
+      'https://lh3.googleusercontent.com/proxy/v4nTT60fEabwQURpI0k_pps9Zy-s330V6pfAuihMi1Wxve91lq5NDVu-B71hctAgLx6a36CjTByFG3HwKld61nNRZeQcgdVi6s6M7C1serxv6pK7_cXdKi-hVXRGu27RFiA=w1200-h630-p-k-no-nu';
 }
