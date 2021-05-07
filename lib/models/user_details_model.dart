b712b4ad1306c5b8d97a73dfbea9a9e1feee4574
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-07 17:25:54 +0200

    provider optimization p.4

--- lib/models/user_details_model.dart
+++ lib/models/user_details_model.dart
@@ -1,7 +1,12 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:BSApp/models/post_model.dart';
+import 'package:BSApp/models/topic_model.dart';
 import 'package:flutter/material.dart';
+
+import 'comment_model.dart';
+import 'deal_model.dart';
 
 class UserModel {
   final String id;
@@ -9,15 +14,23 @@ class UserModel {
   final DateTime registeredAt;
   final DateTime lastLoginAt;
   final DateTime notificationsSeenAt;
+  final List<PostModel> addedPosts;
+  final List<TopicModel> addedTopics;
+  final List<DealModel> addedDeals;
+  final List<CommentModel> addedComments;
   final Uint8List avatar;
 
   UserModel(
       {this.id,
-        this.username,
-        this.registeredAt,
-        this.lastLoginAt,
-        this.notificationsSeenAt,
-        this.avatar});
+      this.username,
+      this.registeredAt,
+      this.lastLoginAt,
+      this.notificationsSeenAt,
+      this.addedPosts,
+      this.addedComments,
+      this.addedDeals,
+      this.addedTopics,
+      this.avatar});
 
   static UserModel fromJson(dynamic userSnapshot) {
     var registeredAt = userSnapshot['registeredAt'];
@@ -26,12 +39,22 @@ class UserModel {
     return UserModel(
         id: userSnapshot['id'],
         username: userSnapshot['username'],
-        registeredAt:
-        registeredAt != null ? DateTime.parse(registeredAt) : null,
+        registeredAt: registeredAt != null ? DateTime.parse(registeredAt) : null,
         lastLoginAt: lastLoginAt != null ? DateTime.parse(lastLoginAt) : null,
-        notificationsSeenAt: notificationsSeenAt != null
-            ? DateTime.parse(notificationsSeenAt)
-            : null,
+        notificationsSeenAt: notificationsSeenAt != null ? DateTime.parse(notificationsSeenAt) : null,
+        // todo
+        addedPosts: (userSnapshot['addedPosts'] as List)
+            .map((e) => PostModel.fromJson(e))
+            .toList(),
+        addedTopics: (userSnapshot['addedTopics'] as List)
+            .map((e) => TopicModel.fromJson(e))
+            .toList(),
+        addedDeals: (userSnapshot['addedDeals'] as List)
+            .map((e) => DealModel.fromJson(e))
+            .toList(),
+        addedComments: (userSnapshot['addedComments'] as List)
+            .map((e) => CommentModel.fromJson(e))
+            .toList(),
         avatar: _getAvatar(userSnapshot));
   }
 
@@ -56,7 +79,7 @@ class UserModel {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is UserModel && runtimeType == other.runtimeType && id == other.id;
+      other is UserModel && runtimeType == other.runtimeType && id == other.id;
 
   @override
   int get hashCode => id.hashCode;
