aabb371543c96479793d5f7572b9390a7ba2b3ec
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-12 12:30:25 +0200

    new topic form handling

--- lib/screens/forum/new_topic_screen.dart
+++ lib/screens/forum/new_topic_screen.dart
@@ -56,6 +56,7 @@ class _NewTopicScreenState extends State<NewTopicScreen> {
   Widget build(BuildContext context) {
     initTopicCategory();
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: BaseAppBar(
         title: 'Nowy temat',
         leading: const AppBarCloseButton(Colors.black),
@@ -73,169 +74,171 @@ class _NewTopicScreenState extends State<NewTopicScreen> {
           )
         ],
       ),
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  FocusScope.of(context).unfocus();
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return FutureBuilder(
-                        future:
-                            Provider.of<TopicCategories>(context, listen: false)
-                                .fetchTopicCategories(),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const Center(child: LoadingIndicator());
-                          } else {
-                            if (snapshot.error != null) {
-                              return const Center(
-                                child: ServerErrorSplash(),
-                              );
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return FutureBuilder(
+                          future:
+                          Provider.of<TopicCategories>(context, listen: false)
+                              .fetchTopicCategories(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const Center(child: LoadingIndicator());
                             } else {
-                              return Consumer<TopicCategories>(
-                                builder: (context, topicCategoriesData, child) {
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 8.0),
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        const Padding(
-                                          padding: EdgeInsets.all(8.0),
-                                          child: Text('Wybierz temat'),
-                                        ),
-                                        ...topicCategoriesData.topicCategories
-                                            .map(
-                                              (e) => GestureDetector(
-                                                onTap: () => _chooseCategory(e),
-                                                child: ListTile(
-                                                  leading: SizedBox(
-                                                    height: 35,
-                                                    width: 35,
-                                                    child: Image.asset(
-                                                      'assets/images/car.png',
-                                                      fit: BoxFit.fitHeight,
-                                                    ),
-                                                  ),
-                                                  title: Text(
-                                                    e.name,
-                                                    style:
-                                                        TextStyle(fontSize: 12),
+                              if (snapshot.error != null) {
+                                return const Center(
+                                  child: ServerErrorSplash(),
+                                );
+                              } else {
+                                return Consumer<TopicCategories>(
+                                  builder: (context, topicCategoriesData, child) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8.0),
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          const Padding(
+                                            padding: EdgeInsets.all(8.0),
+                                            child: Text('Wybierz temat'),
+                                          ),
+                                          ...topicCategoriesData.topicCategories
+                                              .map(
+                                                (e) => GestureDetector(
+                                              onTap: () => _chooseCategory(e),
+                                              child: ListTile(
+                                                leading: SizedBox(
+                                                  height: 35,
+                                                  width: 35,
+                                                  child: Image.asset(
+                                                    'assets/images/car.png',
+                                                    fit: BoxFit.fitHeight,
                                                   ),
                                                 ),
+                                                title: Text(
+                                                  e.name,
+                                                  style:
+                                                  TextStyle(fontSize: 12),
+                                                ),
                                               ),
-                                            )
-                                            .toList(),
-                                      ],
-                                    ),
-                                  );
-                                },
-                              );
+                                            ),
+                                          )
+                                              .toList(),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                );
+                              }
                             }
-                          }
-                        },
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 8.0),
-                  margin: const EdgeInsets.only(bottom: 14.0, top: 6.0),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    border: Border(
-                      bottom:
-                          BorderSide(color: MyColorsProvider.GREY_BORDER_COLOR),
+                          },
+                        );
+                      },
+                    );
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 8.0),
+                    margin: const EdgeInsets.only(bottom: 14.0, top: 6.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                        bottom:
+                        BorderSide(color: MyColorsProvider.GREY_BORDER_COLOR),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            if (topicCategory != null) SizedBox(
+                              height: 25,
+                              width: 25,
+                              child: Image.asset(
+                                'assets/images/car.png',
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                topicCategory != null
+                                    ? topicCategory.name
+                                    : 'Wybierz temat',
+                                style: const TextStyle(
+                                    fontSize: 13, color: Colors.black87),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Icon(
+                          CupertinoIcons.chevron_down,
+                          size: 20,
+                          color: MyColorsProvider.DEEP_BLUE,
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          if (topicCategory != null) SizedBox(
-                            height: 25,
-                            width: 25,
-                            child: Image.asset(
-                              'assets/images/car.png',
-                              fit: BoxFit.fitHeight,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Text(
-                              topicCategory != null
-                                  ? topicCategory.name
-                                  : 'Wybierz temat',
-                              style: const TextStyle(
-                                  fontSize: 13, color: Colors.black87),
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Icon(
-                        CupertinoIcons.chevron_down,
-                        size: 20,
-                        color: MyColorsProvider.DEEP_BLUE,
-                      ),
-                    ],
-                  ),
                 ),
-              ),
-              TextFormField(
-                cursorColor: Colors.black,
-                textInputAction: TextInputAction.next,
-                style: const TextStyle(fontSize: 13),
-                decoration: textFieldDecoration.copyWith(
-                    hintText: 'Tytuł'),
-                controller: _topicTitleController,
-                validator: (value) {
-                  if (value.isEmpty || value.length < 5) {
-                    return 'Tytuł musi mieć przynajmniej 5 znaków!';
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  // _newPassword = value;
-                },
-              ),
-              const Padding(
-                padding: EdgeInsets.all(12.0),
-              ),
-              TextFormField(
-                minLines: 12,
-                maxLines: 12,
-                cursorColor: Colors.black,
-                textInputAction: TextInputAction.next,
-                style: const TextStyle(fontSize: 13),
-                onFieldSubmitted: (_) => FocusScope.of(context).unfocus(),
-                decoration:
-                    textFieldDecoration.copyWith(hintText: 'Napisz coś...'),
-                controller: _topicContentController,
-                validator: (value) {
-                  if (value.isEmpty || value.length < 20) {
-                    return 'Treść musi składać się przynajmniej z 20 znaków!';
-                  } else {
-                    return null;
-                  }
-                },
-                onSaved: (value) {
-                  // _newPassword = value;
-                },
-              )
-            ],
+                TextFormField(
+                  cursorColor: Colors.black,
+                  textInputAction: TextInputAction.next,
+                  style: const TextStyle(fontSize: 13),
+                  decoration: textFieldDecoration.copyWith(
+                      hintText: 'Tytuł'),
+                  controller: _topicTitleController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return 'Tytuł musi mieć przynajmniej 5 znaków!';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    // _newPassword = value;
+                  },
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(12.0),
+                ),
+                TextFormField(
+                  minLines: 12,
+                  maxLines: 12,
+                  cursorColor: Colors.black,
+                  textInputAction: TextInputAction.next,
+                  style: const TextStyle(fontSize: 13),
+                  onFieldSubmitted: (_) => FocusScope.of(context).unfocus(),
+                  decoration:
+                  textFieldDecoration.copyWith(hintText: 'Napisz coś...'),
+                  controller: _topicContentController,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 20) {
+                      return 'Treść musi składać się przynajmniej z 20 znaków!';
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    // _newPassword = value;
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
