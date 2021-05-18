cc32b4fff2c0b732202c6727d791b31f3468014c
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-18 09:21:27 +0200

    open link in browser

--- lib/widgets/deals/deal_item_bottom_section.dart
+++ lib/widgets/deals/deal_item_bottom_section.dart
@@ -1,6 +1,7 @@
 import 'package:BSApp/models/deal_model.dart';
 import 'package:BSApp/widgets/deals/deal_rate_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DealItemBottomSection extends StatelessWidget {
   final DealModel deal;
@@ -21,20 +22,33 @@ class DealItemBottomSection extends StatelessWidget {
             child: DealRateBar(deal.id),
           ),
           Flexible(
-            child: Container(
-              alignment: Alignment.centerRight,
-              child: const Text(
-                'ZOBACZ',
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
+            child: (deal.link != null)
+                ? InkWell(
+                    onTap: () => _launchURL(deal.link),
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      child: const Text(
+                        'ZOBACZ',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(),
           )
         ],
       ),
     );
+  }
+
+  Future _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(Uri.encodeFull(url));
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
