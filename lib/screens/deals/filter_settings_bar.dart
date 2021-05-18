02e95876f175438531d1223bd315bfd8c929eedf
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-18 08:41:29 +0200

    sorting bottom sheet selector

--- lib/screens/deals/filter_settings_bar.dart
+++ lib/screens/deals/filter_settings_bar.dart
@@ -1,5 +1,7 @@
 import 'package:BSApp/models/filter_settings.dart';
+import 'package:BSApp/models/sorting_type.dart';
 import 'package:BSApp/util/my_colors_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'filter_selection_screen.dart';
@@ -66,20 +68,23 @@ class FilterSettingsBar extends StatelessWidget {
           ),
           Flexible(
             // flex: 1,
-            child: Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 6.0),
-              child: Column(
-                children: [
-                  const Text(
-                    'Sortuj',
-                    style: TextStyle(fontSize: 11, color: Colors.grey),
-                  ),
-                  Text(
-                    filterSettings.sortingString,
-                    style: const TextStyle(fontSize: 12, color: MyColorsProvider.DEEP_BLUE),
-                  ),
-                ],
+            child: GestureDetector(
+              onTap: () => _showSortingTypeSelector(context),
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Sortuj',
+                      style: TextStyle(fontSize: 11, color: Colors.grey),
+                    ),
+                    Text(
+                      filterSettings.sortingString,
+                      style: const TextStyle(fontSize: 12, color: MyColorsProvider.DEEP_BLUE),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -96,5 +101,78 @@ class FilterSettingsBar extends StatelessWidget {
         settings: RouteSettings(arguments: filterSettings),
         fullscreenDialog: true));
     updateFunction(newFilterSettings);
+  }
+
+  void _showSortingTypeSelector(BuildContext context) {
+    const titleTextStyle = TextStyle(fontSize: 13, color: Colors.black87);
+    const tileTextStyle = TextStyle(fontSize: 13, color: MyColorsProvider.DEEP_BLUE);
+    const cancelTextStyle = TextStyle(fontSize: 13, color: MyColorsProvider.RED_SHADY);
+    const checkIcon = Icon(Icons.check, color: MyColorsProvider.DEEP_BLUE, size: 20);
+    const paddedIcon = Positioned(top: 12.0, right: 10.0, child: checkIcon);
+    showModalBottomSheet(
+      context: context,
+      // backgroundColor: Colors.transparent,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Center(
+                child: Text('Sortuj po', style: titleTextStyle),
+              ),
+            ),
+            InkWell(
+              onTap: () => _selectSortingType(context, SortingType.NEWEST),
+              child: Container(
+                height: 50.0,
+                alignment: Alignment.center,
+                child: Stack(
+                  children: <Widget>[
+                    const Align(
+                      child: Text('Najnowsze', style: tileTextStyle),
+                    ),
+                    if (filterSettings.sortBy == SortingType.NEWEST) paddedIcon else Container(),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () => _selectSortingType(context, SortingType.MOST_POPULAR),
+              child: Container(
+                height: 50.0,
+                alignment: Alignment.center,
+                child: Stack(
+                  children: <Widget>[
+                    const Align(
+                      child: Text('Najpopularniejsze', style: tileTextStyle),
+                    ),
+                    if (filterSettings.sortBy == SortingType.MOST_POPULAR) paddedIcon else Container(),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () => _selectSortingType(context, null),
+              child: const ListTile(
+                title: Center(
+                  child: Text(
+                    'Anuluj',
+                    style: cancelTextStyle,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _selectSortingType(BuildContext context, SortingType sortingType) {
+    if (sortingType != null) {
+      filterSettings.sortBy = sortingType;
+      updateFunction(filterSettings);
+    }
+    Navigator.pop(context);
   }
 }
