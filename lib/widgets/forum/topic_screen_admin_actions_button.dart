f1794fd0f128854953043d9adee6ce22204787eb
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-21 15:08:17 +0200

    admin actions initial setup

--- lib/widgets/forum/topic_screen_admin_actions_button.dart
+++ lib/widgets/forum/topic_screen_admin_actions_button.dart
@@ -1,6 +1,5 @@
 import 'package:BSApp/models/topic_model.dart';
 import 'package:BSApp/providers/current_user.dart';
-import 'package:BSApp/screens/authentication/auth_screen_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +16,7 @@ class TopicScreenAdminActionsButton extends StatelessWidget {
         final bool isObservedTopic = currentUser.observesTopic(topic);
         return GestureDetector(
           behavior: HitTestBehavior.translucent,
-          onTap: () => _toggleFavourites(context, topic, isObservedTopic, currentUser.isAuthenticated),
+          onTap: () => _openAdminActions(context, topic, isObservedTopic, currentUser.isAuthenticated),
           child: const Padding(
             padding: EdgeInsets.all(8.0),
             child: Icon(CupertinoIcons.ellipsis_vertical, size: 22, color: Colors.black),
@@ -27,14 +26,33 @@ class TopicScreenAdminActionsButton extends StatelessWidget {
     );
   }
 
-  void _toggleFavourites(BuildContext context, TopicModel topic, bool isFavourite, bool isUserLoggedIn) {
-    final currentUserProvider = Provider.of<CurrentUser>(context, listen: false);
-    if (!isUserLoggedIn) {
-      AuthScreenProvider.showLoginScreen(context);
-    } else if (isFavourite) {
-      currentUserProvider.removeFromObservedTopics(topic.id);
-    } else {
-      currentUserProvider.addToObservedTopics(topic.id);
-    }
+  void _openAdminActions(BuildContext context, TopicModel topic, bool isFavourite, bool isUserLoggedIn) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Akcje admina'),
+              ),
+              _buildListTile('Przypnij wątek', CupertinoIcons.lock_fill, () {}),
+              _buildListTile('Usuń wątek', CupertinoIcons.clear, () {}),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _buildListTile(String title, IconData icon, Function() function) {
+    return ListTile(
+      leading: Icon(icon, size: 18,),
+      onTap: function,
+      title: Text(title, style: TextStyle(fontSize: 13),),
+    );
   }
 }
