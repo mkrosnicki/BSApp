c61008f672b6b18486f42c2c3094a3762c0bf149
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-22 13:43:46 +0200

    recolored login app bar

--- lib/screens/authentication/login_registration_screen.dart
+++ lib/screens/authentication/login_registration_screen.dart
@@ -1,16 +1,19 @@
 import 'package:BSApp/util/my_colors_provider.dart';
+import 'package:BSApp/util/my_styling_provider.dart';
 import 'package:BSApp/widgets/authentication/login_form.dart';
 import 'package:BSApp/widgets/authentication/registration_form.dart';
 import 'package:BSApp/widgets/bars/app_bar_close_button.dart';
+import 'package:BSApp/widgets/bars/base_app_bar.dart';
+import 'package:BSApp/widgets/common/decorated_tab_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginRegistrationScreen extends StatefulWidget {
   static const routeName = '/auth';
 
   @override
-  _LoginRegistrationScreenState createState() => _LoginRegistrationScreenState();
+  _LoginRegistrationScreenState createState() =>
+      _LoginRegistrationScreenState();
 }
 
 class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> {
@@ -20,27 +23,25 @@ class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> {
       length: 2,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          elevation: 0.0,
-          brightness: Brightness.light,
-          backwardsCompatibility: false,
-          systemOverlayStyle: const SystemUiOverlayStyle(statusBarColor: MyColorsProvider.DEEP_BLUE),
-          leading: const AppBarCloseButton(Colors.white),
-          backgroundColor: MyColorsProvider.BLUE,
-          bottom: const TabBar(
-            indicatorWeight: 3.0,
-            unselectedLabelStyle: TextStyle(fontSize: 13),
-            indicatorColor: Colors.white,
-            indicatorSize: TabBarIndicatorSize.tab,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white,
-            labelPadding: EdgeInsets.all(10.0),
-            tabs: [
-              Text('Zaloguj się'),
-              Text('Stwórz konto'),
-            ],
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(85.0),
+          child: BaseAppBar(
+            title: 'Witaj',
+            leading: AppBarCloseButton(Colors.black),
+            bottom: DecoratedTabBar(
+              tabBar: TabBar(
+                labelPadding: EdgeInsets.all(10.0),
+                indicatorColor: MyColorsProvider.DEEP_BLUE,
+                labelColor: Colors.black,
+                labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
+                unselectedLabelStyle:
+                    MyStylingProvider.UNSELECTED_TAB_TEXT_STYLE,
+                tabs: [
+                  Text('Zaloguj się'),
+                  Text('Stwórz konto'),
+                ],
+              ),
+            ),
           ),
         ),
         body: TabBarView(
