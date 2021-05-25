b094f39f305ea7b3d5f539406a35f6e5023a6072
mkrosnicki <maciejkrosnicki@gmail.com>
2021-05-25 09:05:58 +0200

    fixed wrong parent bug

--- lib/screens/forum/topic_search_screen.dart
+++ lib/screens/forum/topic_search_screen.dart
@@ -1,19 +1,12 @@
 import 'package:BSApp/providers/categories.dart';
-import 'package:BSApp/providers/current_user.dart';
 import 'package:BSApp/providers/notifications.dart';
 import 'package:BSApp/screens/forum/start_search_topic_button.dart';
-import 'package:BSApp/screens/notifications/clear_notifications_button.dart';
 import 'package:BSApp/util/my_colors_provider.dart';
 import 'package:BSApp/util/my_styling_provider.dart';
-import 'package:BSApp/widgets/bars/app_bar_add_to_observed_searches_button.dart';
 import 'package:BSApp/widgets/bars/app_bar_back_button.dart';
 import 'package:BSApp/widgets/bars/app_bar_bottom_border.dart';
-import 'package:BSApp/widgets/bars/base_app_bar.dart';
 import 'package:BSApp/widgets/common/loading_indicator.dart';
-import 'package:BSApp/widgets/common/login_to_continue_splash.dart';
-import 'package:BSApp/widgets/common/primary_button.dart';
 import 'package:BSApp/widgets/common/server_error_splash.dart';
-import 'package:BSApp/widgets/notifications/notifications_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -109,38 +102,35 @@ class _TopicSearchScreenState extends State<TopicSearchScreen> {
     return Container(
       width: double.infinity,
       alignment: Alignment.centerLeft,
-      child:           Expanded(
-        child: TextField(
-          controller: _searchTextController,
-          onSubmitted: (phrase) {
-            // filterSettings.phrase = phrase;
-            // _updateFilterSettings(filterSettings);
-          },
-          keyboardType: TextInputType.text,
-          style: const TextStyle(fontSize: 12),
-          decoration: MyStylingProvider.TEXT_FIELD_DECORATION.copyWith(
-            hintText: 'Czego szukasz?',
-            prefixIcon: const Padding(
-              padding: EdgeInsets.all(0.0),
-              child: Icon(
-                CupertinoIcons.search,
-                color: Colors.black54,
-                size: 15,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _searchTextController,
+              onSubmitted: (phrase) {
+                // filterSettings.phrase = phrase;
+                // _updateFilterSettings(filterSettings);
+              },
+              keyboardType: TextInputType.text,
+              style: const TextStyle(fontSize: 12),
+              decoration: MyStylingProvider.TEXT_FIELD_DECORATION.copyWith(
+                hintText: 'Czego szukasz?',
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: Icon(
+                    CupertinoIcons.search,
+                    color: Colors.black54,
+                    size: 15,
+                  ),
+                ),
+                prefixIconConstraints: BoxConstraints.tight(
+                  const Size.square(30),
+                ),
               ),
             ),
-            prefixIconConstraints: BoxConstraints.tight(
-              const Size.square(30),
-            ),
           ),
-        ),
+        ],
       ),
     );
-  }
-
-  @override
-  void deactivate() {
-    Provider.of<Notifications>(context, listen: false).updateNotificationsTimestamp();
-    Provider.of<CurrentUser>(context, listen: false).updateNotificationsTimestamp();
-    super.deactivate();
   }
 }
