3fdb0fd64f1523d1c96d9c8bc115aee5bfc19639
mkrosnicki <maciejkrosnicki@gmail.com>
2021-06-09 12:00:46 +0200

    initial about page

--- lib/screens/profile/about_app_screen.dart
+++ lib/screens/profile/about_app_screen.dart
@@ -1,17 +1,16 @@
-import 'package:BSApp/models/custom_exception.dart';
-import 'package:BSApp/providers/auth.dart';
 import 'package:BSApp/util/my_colors_provider.dart';
 import 'package:BSApp/widgets/bars/app_bar_back_button.dart';
 import 'package:BSApp/widgets/bars/base_app_bar.dart';
 import 'package:BSApp/widgets/common/form_field_divider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutAppScreen extends StatelessWidget {
   static const routeName = '/about-app';
 
   static const TextStyle titleStyle = TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black);
-  static const TextStyle valueStyle = TextStyle(fontSize: 15, color: MyColorsProvider.DEEP_BLUE, fontWeight: FontWeight.w600);
+  static const TextStyle valueStyle =
+      TextStyle(fontSize: 15, color: MyColorsProvider.DEEP_BLUE, fontWeight: FontWeight.w600);
 
   @override
   Widget build(BuildContext context) {
@@ -25,20 +24,43 @@ class AboutAppScreen extends StatelessWidget {
         padding: const EdgeInsets.all(12.0),
         width: double.infinity,
         child: Column(
-          children: const [
-            FormFieldDivider(),
-            Text('Wersja aplikacji', style: titleStyle,),
-            FormFieldDivider(),
-            Text('0.9', style: valueStyle,),
-            FormFieldDivider(),
-            FormFieldDivider(),
-            FormFieldDivider(),
-            Text('Ikony pochodzą ze strony', style: titleStyle,),
-            FormFieldDivider(),
-            Text('https://www.pngrepo.com/', style: valueStyle,),
+          children: [
+            const FormFieldDivider(),
+            const Text(
+              'Wersja aplikacji',
+              style: titleStyle,
+            ),
+            const FormFieldDivider(),
+            const Text(
+              '0.9',
+              style: valueStyle,
+            ),
+            const FormFieldDivider(),
+            const FormFieldDivider(),
+            const FormFieldDivider(),
+            const Text(
+              'Ikony pochodzą ze strony',
+              style: titleStyle,
+            ),
+            const FormFieldDivider(),
+            InkWell(
+              onTap: () => _launchURL('https://www.pngrepo.com'),
+              child: const Text(
+                'PNG Repo',
+                style: valueStyle,
+              ),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Future _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(Uri.encodeFull(url));
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
