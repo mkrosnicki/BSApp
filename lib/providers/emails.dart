1a207d6629f6a0efa19462c8b37a07e0caf3d76e
mkrosnicki <maciejkrosnicki@gmail.com>
2021-07-14 14:07:54 +0200

    seding contact emails

--- lib/providers/emails.dart
+++ lib/providers/emails.dart
@@ -8,12 +8,16 @@ class Emails with ChangeNotifier {
   final ApiProvider _apiProvider = ApiProvider();
 
   Future<bool> sendEmail(final String subject, final String content, final String authorEmail) async {
-    final responseBody = await _apiProvider.post('/emails', {
-      'subject': subject,
-      'content': content,
-      'authorEmail': authorEmail
-    });
-    return responseBody == 'Sent';
+    try {
+      final responseBody = await _apiProvider.post('/emails', {
+        'subject': subject,
+        'content': content,
+        'authorEmail': authorEmail
+      });
+      return responseBody == 'Sent';
+    } catch (e) {
+      return false;
+    }
   }
 
 }
