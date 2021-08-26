95015141607d82a11308a1ca783b7a5a4220b33e
mkrosnicki <maciejkrosnicki@gmail.com>
2021-08-26 13:24:11 +0200

    fixed not updating tab bar

--- lib/screens/authentication/login_registration_screen.dart
+++ lib/screens/authentication/login_registration_screen.dart
@@ -8,6 +8,7 @@ import 'package:BSApp/widgets/bars/base_app_bar.dart';
 import 'package:BSApp/widgets/common/colored_tab.dart';
 import 'package:BSApp/widgets/common/decorated_tab_bar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginRegistrationScreen extends StatefulWidget {
@@ -17,46 +18,64 @@ class LoginRegistrationScreen extends StatefulWidget {
   _LoginRegistrationScreenState createState() => _LoginRegistrationScreenState();
 }
 
-class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> {
+class _LoginRegistrationScreenState extends State<LoginRegistrationScreen> with TickerProviderStateMixin  {
+
   int _selectedIndex = 0;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _tabController.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController.index;
+        _tabController.animateTo(_selectedIndex, duration: Duration.zero);
+      });
+    });
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80.0),
-          child: BaseAppBar(
-            title: 'Witaj',
-            leading: AppBarCloseButton(Colors.white),
-            bottom: DecoratedTabBar(
-              tabBar: TabBar(
-                labelPadding: EdgeInsets.zero,
-                indicatorColor: MyColorsProvider.PASTEL_LIGHT_BLUE,
-                labelColor: Colors.white,
-                labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
-                unselectedLabelStyle: MyStylingProvider.UNSELECTED_TAB_TEXT_STYLE,
-                onTap: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                tabs: [
-                  ColoredTab('Zaloguj się', _selectedIndex == 0),
-                  ColoredTab('Stwórz konto', _selectedIndex == 1),
-                ],
-              ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0),
+        child: BaseAppBar(
+          title: 'Witaj',
+          leading: AppBarCloseButton(Colors.white),
+          bottom: DecoratedTabBar(
+            tabBar: TabBar(
+              controller: _tabController,
+              labelPadding: EdgeInsets.zero,
+              indicatorColor: MyColorsProvider.PASTEL_LIGHT_BLUE,
+              labelColor: Colors.white,
+              labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
+              unselectedLabelStyle: MyStylingProvider.UNSELECTED_TAB_TEXT_STYLE,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                ColoredTab('Zaloguj się', _selectedIndex == 0),
+                ColoredTab('Stwórz konto', _selectedIndex == 1),
+              ],
             ),
           ),
         ),
-        body: TabBarView(
-          children: [
-            LoginForm(),
-            RegistrationForm(),
-          ],
-        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          LoginForm(),
+          RegistrationForm(),
+        ],
       ),
     );
   }
