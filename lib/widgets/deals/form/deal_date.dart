67325ed9bb8869aa41e50c0d32e49daa2257e5db
mkrosnicki <maciejkrosnicki@gmail.com>
2021-08-26 11:37:11 +0200

    dates validation

--- lib/widgets/deals/form/deal_date.dart
+++ lib/widgets/deals/form/deal_date.dart
@@ -11,12 +11,24 @@ class DealDateSelector {
     DealDateType dateType,
   ) async {
     final initialDate = _getInitialDate(deal, dateType);
+    final theOtherDate = _getTheOtherDate(deal, dateType);
 
     final DateTime picked = await showDatePicker(
       context: context,
       initialDate: initialDate,
       firstDate: DateTime(2020),
       lastDate: DateTime(2025),
+      selectableDayPredicate: (date) {
+        if (theOtherDate == null) {
+          return true;
+        } else if (dateType == DealDateType.VALID_FROM) {
+          return date.isBefore(theOtherDate) || date.isAtSameMomentAs(theOtherDate);
+        } else if (dateType == DealDateType.VALID_TO) {
+          return date.isAfter(theOtherDate) || date.isAtSameMomentAs(theOtherDate);
+        } else {
+          return true;
+        }
+      },
     );
     if (picked != null) {
         if (dateType == DealDateType.VALID_TO) {
@@ -26,6 +38,16 @@ class DealDateSelector {
         }
     }
     return Future.value();
+  }
+
+  static DateTime _getTheOtherDate(AddDealModel deal, DealDateType dateType) {
+    DateTime theOtherDate;
+    if (dateType == DealDateType.VALID_TO) {
+      theOtherDate = deal.validFrom;
+    } else if (dateType == DealDateType.VALID_FROM) {
+      theOtherDate = deal.validTo;
+    }
+    return theOtherDate;
   }
 
   static DateTime _getInitialDate(AddDealModel deal, DealDateType dateType) {
