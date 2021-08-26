5c42249678fd1355b7b7e22103803de3a0dc70db
mkrosnicki <maciejkrosnicki@gmail.com>
2021-08-26 13:27:27 +0200

    fixed not updating tab bar p2

--- lib/screens/favourites/favourites_screen.dart
+++ lib/screens/favourites/favourites_screen.dart
@@ -14,45 +14,62 @@ class FavouritesScreen extends StatefulWidget {
   _FavouritesScreenState createState() => _FavouritesScreenState();
 }
 
-class _FavouritesScreenState extends State<FavouritesScreen> {
+class _FavouritesScreenState extends State<FavouritesScreen> with TickerProviderStateMixin{
 
   int _selectedIndex = 0;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _tabController.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController.index;
+        _tabController.animateTo(_selectedIndex, duration: Duration.zero);
+      });
+    });
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80.0),
-          child: BaseAppBar(
-            title: 'Ulubione',
-            bottom: DecoratedTabBar(
-              tabBar: TabBar(
-                labelPadding: EdgeInsets.zero,
-                indicatorColor: MyColorsProvider.PASTEL_LIGHT_BLUE,
-                labelColor: Colors.white,
-                labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
-                unselectedLabelStyle: MyStylingProvider.UNSELECTED_TAB_TEXT_STYLE,
-                onTap: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                tabs: [
-                  ColoredTab('Okazje', _selectedIndex == 0),
-                  ColoredTab('Wyszukiwania', _selectedIndex == 1),
-                ],
-              ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: BaseAppBar(
+          title: 'Ulubione',
+          bottom: DecoratedTabBar(
+            tabBar: TabBar(
+              controller: _tabController,
+              labelPadding: EdgeInsets.zero,
+              indicatorColor: MyColorsProvider.PASTEL_LIGHT_BLUE,
+              labelColor: Colors.white,
+              labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
+              unselectedLabelStyle: MyStylingProvider.UNSELECTED_TAB_TEXT_STYLE,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                ColoredTab('Okazje', _selectedIndex == 0),
+                ColoredTab('Wyszukiwania', _selectedIndex == 1),
+              ],
             ),
           ),
         ),
-        body: TabBarView(
-          children: [
-            ObservedDealsView(),
-            ObservedSearchesView(),
-          ],
-        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ObservedDealsView(),
+          ObservedSearchesView(),
+        ],
       ),
     );
   }
--- lib/screens/forum/forum_screen.dart
+++ lib/screens/forum/forum_screen.dart
@@ -16,48 +16,65 @@ class ForumScreen extends StatefulWidget {
   _ForumScreenState createState() => _ForumScreenState();
 }
 
-class _ForumScreenState extends State<ForumScreen> {
+class _ForumScreenState extends State<ForumScreen> with TickerProviderStateMixin {
   int _selectedIndex = 0;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _tabController.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController.index;
+        _tabController.animateTo(_selectedIndex, duration: Duration.zero);
+      });
+    });
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(80.0),
-          child: BaseAppBar(
-            title: 'Forum',
-            leading: const AppBarSearchTopicButton(),
-            bottom: DecoratedTabBar(
-              tabBar: TabBar(
-                labelPadding: EdgeInsets.zero,
-                indicatorColor: Colors.transparent,
-                labelColor: Colors.white,
-                labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
-                unselectedLabelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
-                onTap: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                tabs: [
-                  ColoredTab('Kategorie', _selectedIndex == 0),
-                  ColoredTab('Obserwowane tematy', _selectedIndex == 1),
-                ],
-              ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: BaseAppBar(
+          title: 'Forum',
+          leading: const AppBarSearchTopicButton(),
+          bottom: DecoratedTabBar(
+            tabBar: TabBar(
+              controller: _tabController,
+              labelPadding: EdgeInsets.zero,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.white,
+              labelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
+              unselectedLabelStyle: MyStylingProvider.SELECTED_TAB_TEXT_STYLE,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              tabs: [
+                ColoredTab('Kategorie', _selectedIndex == 0),
+                ColoredTab('Obserwowane tematy', _selectedIndex == 1),
+              ],
             ),
-            actions: const [
-              AppBarAddTopicButton(null),
-            ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            ForumCategoriesView(),
-            ForumMyTopicsView(),
+          actions: const [
+            AppBarAddTopicButton(null),
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ForumCategoriesView(),
+          ForumMyTopicsView(),
+        ],
       ),
     );
   }
