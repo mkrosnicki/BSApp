5c29db155c3e1906041e6db50ac919c572aba6ad
mkrosnicki <maciejkrosnicki@gmail.com>
2021-08-30 10:22:28 +0200

    share button doesn't seem to disappear

--- lib/widgets/deals/deal_item_share_button.dart
+++ lib/widgets/deals/deal_item_share_button.dart
@@ -1,11 +1,8 @@
 import 'package:BSApp/models/deal_model.dart';
 import 'package:BSApp/services/dynamic_link_service.dart';
-import 'package:BSApp/providers/current_user.dart';
-import 'package:BSApp/screens/authentication/auth_screen_provider.dart';
 import 'package:BSApp/util/my_colors_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:share/share.dart';
 
 class DealItemShareButton extends StatelessWidget {
@@ -20,7 +17,7 @@ class DealItemShareButton extends StatelessWidget {
     return FutureBuilder<Uri>(
         future: dynamicLinkService.createDynamicLink(deal.id),
         builder: (context, snapshot) {
-          if(snapshot.hasData) {
+          if (snapshot.hasData) {
             final Uri uri = snapshot.data;
             return GestureDetector(
               behavior: HitTestBehavior.translucent,
@@ -28,37 +25,8 @@ class DealItemShareButton extends StatelessWidget {
               child: Icon(CupertinoIcons.share, size: iconSize, color: MyColorsProvider.DEEP_BLUE),
             );
           } else {
-            return Container();
+            return Icon(CupertinoIcons.share, size: iconSize, color: MyColorsProvider.DEEP_BLUE);
           }
-
-        }
-    );
-    return Consumer<CurrentUser>(
-      builder: (context, currentUser, child) {
-        final bool isObservedDeal = currentUser.observesDeal(deal);
-        return GestureDetector(
-          onTap: () => _toggleFavourites(
-              context, deal, isObservedDeal, currentUser.isAuthenticated),
-          child: isObservedDeal
-              ? Icon(CupertinoIcons.share,
-              size: iconSize, color: MyColorsProvider.LIGHT_RED_SHADY)
-              : Icon(CupertinoIcons.heart_fill,
-              size: iconSize, color: MyColorsProvider.LIGHT_GRAY),
-        );
-      },
-    );
-  }
-
-  void _toggleFavourites(BuildContext context, DealModel deal, bool isFavourite,
-      bool isUserLoggedIn) {
-    final currentUserProvider =
-        Provider.of<CurrentUser>(context, listen: false);
-    if (!isUserLoggedIn) {
-      AuthScreenProvider.showLoginScreen(context);
-    } else if (isFavourite) {
-      currentUserProvider.removeFromObservedDeals(deal.id);
-    } else {
-      currentUserProvider.addToObservedDeals(deal.id);
-    }
+        });
   }
 }
